767098b65333448c5eba5e6b6c0d9bc47624d17a
kowalks <guilhermekowalczuk@gmail.com>
2021-06-22 05:36:02 -0300

    Fixed some bugs

--- spotify/urls.py
+++ spotify/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('get-auth-url', AuthURL.as_view()),
     path('callback', callback),
     path('is-authenticated', IsAuthenticated.as_view()),
-    path('current-song', CurrentSong.as_view())
+    path('current-song', CurrentSong.as_view()),
+    path('top-tracks', TopTracks.as_view()),
+    path('recibofy', Recibofy.as_view()),
 ]
