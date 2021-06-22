5d2f49d82945223015e32965e05891019f3d059a
kowalks <guilhermekowalczuk@gmail.com>
2021-06-22 05:45:53 -0300

    Added missing apps on django

--- core/settings.py
+++ core/settings.py
@@ -37,8 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playlist',
-    'music',
+    'spotify.apps.SpotifyConfig',
+    'frontend',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
