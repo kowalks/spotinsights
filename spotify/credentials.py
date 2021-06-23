9569f47bfd5b3a2feb3c38f3eacf681e34ebc73c
kowalks <guilhermekowalczuk@gmail.com>
2021-06-23 15:32:23 -0300

    reset default credential settings

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,4 @@
 CLIENT_ID = 'e213828054d447cc9cc71a176350fe99'
 CLIENT_SECRET = '3b7d42078d0e48e3aa600efe8e1ff22e'
-REDIRECT_URI = 'https://spotinsights.herokuapp.com/spotify/callback'
+# REDIRECT_URI = 'https://spotinsights.herokuapp.com/spotify/callback'
+REDIRECT_URI = 'https://127.0.0.1/spotify/callback'
