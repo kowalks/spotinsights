ea931d384a7c3fb0185734392b618df1d176763b
Yuri Gama <63912211+yuri-gama@users.noreply.github.com>
2021-06-23 20:17:20 -0300

    credentials updated

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,4 +1,4 @@
 CLIENT_ID = 'e213828054d447cc9cc71a176350fe99'
 CLIENT_SECRET = '3b7d42078d0e48e3aa600efe8e1ff22e'
 # REDIRECT_URI = 'https://spotinsights.herokuapp.com/spotify/callback'
-REDIRECT_URI = 'https://127.0.0.1/spotify/callback'
+REDIRECT_URI = 'http://127.0.0.1:8000/spotify/callback'
