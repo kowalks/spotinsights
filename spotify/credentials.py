09dd2064a0f0e6c1b7a29877bb76655f00eee227
kowalks <guilhermekowalczuk@gmail.com>
2021-06-23 12:38:47 -0300

    deploy test 4

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,3 @@
 CLIENT_ID = 'e213828054d447cc9cc71a176350fe99'
 CLIENT_SECRET = '3b7d42078d0e48e3aa600efe8e1ff22e'
-REDIRECT_URI = 'http://127.0.0.1:8000/spotify/callback'
+REDIRECT_URI = 'https://spotinsights.herokuapp.com/callback'
