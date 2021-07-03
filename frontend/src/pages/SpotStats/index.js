4de593e70cc0428b17146ee84edfb1984e4e2fd0
FernandoZanchitta <50930741+FernandoZanchitta@users.noreply.github.com>
2021-07-03 15:15:30 -0300

    Adding new labels to songChart

--- frontend/src/pages/SpotStats/index.js
+++ frontend/src/pages/SpotStats/index.js
@@ -11,11 +11,16 @@ import './styles.css';
 export default function SpotStats() {
     const [data, setData] = useState([]);
     let title = " SpotStats ";
-    const generalTypeData = [
+    const generalTypeData = 
+        [
         {'label': 'Happy', 'value': '67'},
         {'label':'Energy', 'value': '54'},
         {'label':'Danceable', 'value': '45'},
-        {'label':'Acoustic', 'value': '34'}];
+        {'label':'Acoustic', 'value': '34'},
+        {'label':'Speech', 'value': '80'},
+        {'label':'Instrumental', 'value': '12'},
+        {'label':'Life', 'value': '60'}
+        ];
     const loadData = async () => {
         try{
             // await fetch('/spotify/top-tracks?limit=3');
@@ -64,103 +69,3 @@ export default function SpotStats() {
         </Container>
         );
 }
-
-
- // <div>
-        //     <h1>SpotStats</h1>
-        //     <Container  classy = 'algo'>
-        //         <p>Hello World!</p> 
-        //     </Container>
-        //     <XYPlot
-        //         yType={'ordinal'}
-        //         width={450}
-        //         height={300}>
-        //         <HorizontalBarSeries 
-        //           data={myData}
-        //           onValueMouseOver={(d) => {console.log(d);}} 
-        //           colorType="literal"/>
-        //           <XAxis />
-        //           <YAxis />
-        //     </XYPlot>
-        //     <Grid container direction="row" justify="center" alignItems="flex-end">
-        //     <Card title = "Spotstats" string = "Verifique sua estátistica musical em tempo real!" img= "../static/img/chart-icon.png"/>
-        //     <Card title = "Recibofy" string = "Compartilhe seus resultados de uma forma criativa!" img="../static/img/path-icon.png"/>
-        //     <Card title = "FeatPath" string = "Crie Playlists Personalizadas Linkando artistas!" img="../static/img/receipt-icon.png"/>
-        //     </Grid>
-        // </div>
-
-
-
-
-    // const myData = [
-    //     {
-    //         "name": "Saib",
-    //         "position": 1,
-    //         "rating": 64,
-    //         "artist_id": "6N4HlHINMvoTyAL0yhBUCk",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Matuê",
-    //         "position": 2,
-    //         "rating": 75,
-    //         "artist_id": "5nP8x4uEFjAAmDzwOEc9b8",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "The Weeknd",
-    //         "position": 3,
-    //         "rating": 96,
-    //         "artist_id": "1Xyo4u8uXC1ZmMpatF05PJ",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Bee Gees",
-    //         "position": 4,
-    //         "rating": 80,
-    //         "artist_id": "1LZEQNv7sE11VDY3SdxQeN",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Michael Kiwanuka",
-    //         "position": 5,
-    //         "rating": 70,
-    //         "artist_id": "0bzfPKdbXL5ezYW2z3UGQj",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Pineapple StormTv",
-    //         "position": 6,
-    //         "rating": 76,
-    //         "artist_id": "09U6hmCerKcIJrixubiBjm",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Post Malone",
-    //         "position": 7,
-    //         "rating": 92,
-    //         "artist_id": "246dkjvS1zLTtiykXe5h60",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Costa Gold",
-    //         "position": 8,
-    //         "rating": 68,
-    //         "artist_id": "7q1aEytv83jXNECmyaMhgn",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Pink Floyd",
-    //         "position": 9,
-    //         "rating": 83,
-    //         "artist_id": "0k17h0D3J5VfsdmQ1iZtE9",
-    //         "color": 'red'
-    //     },
-    //     {
-    //         "name": "Teto",
-    //         "position": 10,
-    //         "rating": 70,
-    //         "artist_id": "68YeXpLt3jB7JHQS5ZjMGo",
-    //         "color": 'red'
-    //     }
-    // ];
